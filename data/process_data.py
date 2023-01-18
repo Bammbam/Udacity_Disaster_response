68fec5686c8b545f660e1e52a755d812e4e43f44
Bammbam <rachatachankanasuk@gmail.com>
2023-01-18 23:21:03 +0700

    First submission v1

---
+++ data/process_data.py
@@ -0,0 +1,57 @@
+import sys
+import pandas as pd
+import re
+from sqlalchemy import create_engine
+
+
+def load_data(messages_filepath, categories_filepath):
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    return messages, categories
+
+
+def clean_data(message_df, category_df):
+    category_df = pd.concat([category_df.drop(columns=['categories']) , category_df['categories'].str.split(';', expand=True)], axis=1)
+    cat_cols = category_df.iloc[0,1:].values
+    cat_cols = [col.split('-')[0] for col in cat_cols] 
+    category_df.columns = ['id'] + cat_cols
+    for col in cat_cols:
+        category_df[col] = category_df[col].apply(lambda x: x.split('-')[1]).astype('int64')
+    df = message_df.merge(category_df, how='left', left_on='id', right_on='id')
+
+    # There're some duplicated data, thus we'll eliminate it first
+    df = df.drop_duplicates() 
+    return df
+
+
+
+def save_data(df, database_filename, table_name):
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql(f'{table_name}', engine, index=False)
+    pass 
+
+
+def main():
+    try:
+
+        messages_filepath, categories_filepath, database_filepath = 'messages.csv','categories.csv','Disaster_response.db'
+        table_name = 'response_message'
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        message_df, category_df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(message_df, category_df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath, table_name)
+        
+        print('Cleaned data saved to database!')
+    
+    except:
+        print("Something went wrong along the pipeline. Use log above to see where it went wrong")
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/train_classifier.py
@@ -0,0 +1,115 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import numpy as np
+import pickle
+
+import nltk
+from nltk.tokenize import word_tokenize
+import re
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.metrics import classification_report
+
+from textblob import TextBlob
+
+
+rel_database_filepath = '../data/Disaster_response.db'
+
+def load_data(rel_database_filepath):
+    engine = create_engine(f'sqlite:///{rel_database_filepath}')
+    df = pd.read_sql_query("SELECT * FROM response_message", engine)
+    # 'related' columns has value = 2 which is usual ==> replace it with 1
+    df['related'] = np.where(df['related']==2, 1,df['related'])  
+    return df['message'], df[df.columns[4:]], df.columns[4:]
+
+
+def tokenize(text):
+    lemmatizer = WordNetLemmatizer()
+    stemmer = PorterStemmer()
+    orig_text = text
+    text = re.sub('[^A-Za-z0-9]',' ', text)
+    text = text.lower()
+    tok_texts = word_tokenize(text) 
+    text = [stemmer.stem(lemmatizer.lemmatize(w.strip())) for w in tok_texts if w.strip() not in stopwords.words('english')]
+    return text
+
+
+class TextPolarizer(BaseEstimator, TransformerMixin):
+
+    def getPolarity(self, text):
+        return TextBlob(text).sentiment.polarity
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.getPolarity)
+        return pd.DataFrame(X_tagged)
+
+
+def build_model():
+    pipeline = Pipeline(steps=[
+                   ('features', FeatureUnion([
+                       (('tfidf', TfidfVectorizer(tokenizer=tokenize))),
+                       ('txt_polar', TextPolarizer())
+                   ])),
+                   ('clf', MultiOutputClassifier(AdaBoostClassifier()))
+        ])
+    """Example code to show that I can use GridSearch to tune Parameter."""
+    # parameters = {'clf__estimator__min_samples_leaf': [1,2], 
+    #             'clf__estimator__min_samples_split': [2,3]}
+
+    # model = GridSearchCV(pipeline, param_grid=parameters)
+    return pipeline
+
+
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    print(classification_report(y_pred=model.predict(X_test), y_true=Y_test, target_names=category_names))
+    pass
+
+
+def save_model(model, model_filepath):
+    pickle.dump(model, open(model_filepath, 'wb'))
+    pass
+
+
+def main():
+    try:
+        rel_database_filepath, model_filepath = '../data/Disaster_response.db', './clf_model.pkl'
+        print('Loading data...\n    DATABASE: {}'.format(rel_database_filepath))
+        X, Y, category_names = load_data(rel_database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        # print("checking category name: ", category_names)
+
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    except:
+        print("Something went wrong along the pipeline. Use log above to see where it went wrong")
+
+
+if __name__ == '__main__':
+    main()
